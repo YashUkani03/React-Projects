15a499177f597ad946f4ecca7ecaf67c871aef7f
Yash Patel <110323334+yash-ukani@users.noreply.github.com>
2024-07-02 17:23:34 +0530

    new project start

---
+++ 13_tasks/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='text-3xl text-center'>Hello Everyone</div>
+    
+  );
+}
+
+export default App;
