0a6939d876cd13fa45385b0bd01ddbcb381fe579
Yash Patel <110323334+yash-ukani@users.noreply.github.com>
2024-07-09 19:34:13 +0530

    Commited Changes

--- 05_currencyconverter/src/App.js
+++ 05_currencyconverter/src/App.js
@@ -28,9 +28,6 @@ function App() {
   return (
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
-      style={{
-        backgroundImage: `url('https://images.pexels.com/photos/206359/pexels-photo-206359.jpeg?auto=compress&cs=tinysrgb&w=600')`
-      }}
     >
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
